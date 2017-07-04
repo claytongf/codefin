ec67a4d18ba8a7069925e8b17194686d3f1e7608
Clayton Figueiredo <clayton@techwing.com.br>
2017-07-04 15:15:27 +0000

    Erros nas rotas de criação, edição e exclusão de contas bancárias

---
+++ resources/assets/_default/components/PageTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="card-panel green lighten-3">
+        <span class="green-text text-darken-2">
+            <slot></slot>
+        </span>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default{
+    }
+</script>
